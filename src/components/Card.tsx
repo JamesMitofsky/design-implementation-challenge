1457ebdde5e4b26f13ea032a8ef4967dc2a44b2f
James Mitofsky <JamesMitofsky@gmail.com>
2022-11-09 13:11:27 +0100

    Move card content into a dedicated component "CardText"

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,23 @@
 import { NFTStructure } from "../types/NFTObjectType";
-import { Card as MUICard, CardContent, CardMedia } from "@mui/material";
+import { Card as MUICard } from "@mui/material";
 import { Image } from "mui-image";
+import CardText from "./CardText";
 
 // maxwidth: 225px
 
 const Card = (props: NFTStructure) => {
-  console.log(props.thumbnail);
   return (
-    <MUICard sx={{ minWidth: 225, maxWidth: 225, height: 400 }}>
-      <Image src={props.thumbnail} alt="Preview of NFT for sale" height={200} />
-      <CardContent>{props.name}</CardContent>
+    <MUICard
+      sx={{
+        minWidth: 225,
+        maxWidth: 225,
+        height: 400,
+        borderRadius: 3,
+      }}
+    >
+      <Image src={props.thumbnail} alt="Preview of NFT for sale" height={300} />
+
+      <CardText {...props} />
     </MUICard>
   );
 };
