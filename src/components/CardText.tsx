03ef6b205d3be818ab577b1eca794c1ce81d5a2e
James Mitofsky <JamesMitofsky@gmail.com>
2022-11-10 20:46:46 +0100

    Add Grid to card text; begin to align with mockup

--- src/components/CardText.tsx
+++ src/components/CardText.tsx
@@ -1,18 +1,29 @@
 import { NFTStructure } from "../types/NFTObjectType";
 import { CardContent, Typography } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2";
 
 const CardText = (props: NFTStructure) => {
   console.log(props);
   return (
     <CardContent>
-      <Typography variant="h6" component="div">
-        {props.name}
-      </Typography>
-      <Typography component="div">Price</Typography>
-      <Typography component="div">
-        {props?.sale?.unitary_price_float}
-      </Typography>
-      <Typography component="div">Best Offer</Typography>
+      <Grid2 container>
+        <Grid2 xs={12}>
+          <Typography sx={{ fontSize: 10 }} component="h2">
+            {props.name}
+          </Typography>
+        </Grid2>
+        <Grid2 xs={6}>
+          <Typography variant="subtitle1" component="div">
+            Price
+          </Typography>
+          <Typography sx={{ fontSize: 10 }}>
+            {props?.sale?.unitary_price_float.toFixed(1)}
+          </Typography>
+        </Grid2>
+        <Grid2 xs={6}>
+          <Typography variant="subtitle1">Best Offer</Typography>
+        </Grid2>
+      </Grid2>
     </CardContent>
   );
 };
