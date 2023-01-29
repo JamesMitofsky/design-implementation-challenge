0016f8a6285703bc093e8a1526c42156fc508a82
James Mitofsky <JamesMitofsky@gmail.com>
2023-01-29 16:29:36 +0100

    Remove dividers & standardize avatar usage

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,20 @@
 import { AppBar, Toolbar } from "@mui/material";
 import { Image } from "mui-image";
 import Logo from "../assets/logo.png";
-import ImageIcon from "./ImageIcon";
+import AvatarImage from "./AvatarImage";
 import UserImage from "../assets/user.png";
 import { Box } from "@mui/system";
-import Divider from "./Divider";
 
 const NavBar = () => {
   return (
-    <>
-      <AppBar color="inherit" position="static" elevation={0}>
-        <Toolbar>
-          <Box sx={{ flexGrow: 1 }}>
-            <Image src={Logo} alt="logo" width={20} />
-          </Box>
-          <ImageIcon profileImage={UserImage} />
-        </Toolbar>
-      </AppBar>
-      <Divider />
-    </>
+    <AppBar color="inherit" position="static">
+      <Toolbar>
+        <Box sx={{ flexGrow: 1 }}>
+          <Image src={Logo} alt="logo" width={20} />
+        </Box>
+        <AvatarImage imageURL={UserImage} />
+      </Toolbar>
+    </AppBar>
   );
 };
 export default NavBar;
--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,5 +1,4 @@
 import { Box, Typography } from "@mui/material";
-import Divider from "../components/Divider";
 
 const PageTitle = ({
   title,
@@ -9,25 +8,22 @@ const PageTitle = ({
   subtitle: string;
 }) => {
   return (
-    <Box sx={{ mb: 3.5 }}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          textAlign: "center",
-          maxWidth: 300,
-          margin: "auto",
-          mt: 4,
-          mb: 5,
-        }}
-      >
-        <Typography variant="h1" sx={{ mb: 2 }}>
-          {title}
-        </Typography>
-        <Typography variant="subtitle1">{subtitle}</Typography>
-      </Box>
-      <Divider />
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+        maxWidth: 300,
+        margin: "auto",
+        mt: 4,
+        mb: 5,
+      }}
+    >
+      <Typography variant="h1" sx={{ mb: 2 }}>
+        {title}
+      </Typography>
+      <Typography variant="subtitle1">{subtitle}</Typography>
     </Box>
   );
 };
